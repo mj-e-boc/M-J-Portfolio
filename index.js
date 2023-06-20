61d077bd3bb70b2b23800f1e8c7d8ab8dbfaa09f
Ronald Mjonono <mjay@M-JAY.local>
2023-06-20 23:50:38 +0200

    fix js

--- index.js
+++ index.js
@@ -1,26 +1,19 @@
-const cancelButton = document.querySelector(".cancelicon");
-const hamburger = document.querySelector(".bars");
-const navMenu = document.querySelector(".navMenu");
-const menuItem = document.getElementsByClassName("menuItem");
-const activePage = document.querySelectorAll;
-
-cancelButton.addEventListener("click", myFunction);
+const cancelButton = document.querySelector('.cancelicon');
+const hamburger = document.querySelector('.bars');
+const navMenu = document.querySelector('.navMenu');
 
 function myFunction() {
-  navMenu.style.left = "200%";
+  navMenu.style.left = '200%';
 }
 
-hamburger.addEventListener("click", hideMenu);
+cancelButton.addEventListener('click', myFunction);
 
 function hideMenu() {
-  navMenu.style.left = "0";
+  navMenu.style.left = '0';
 }
 
-document.querySelectorAll(".navMenu li").forEach((n) =>
-  n.addEventListener("click", function () {
-    navMenu.style.left = "200%";
-    if ((n = 0)) {
-      window.location = "google.com";
-    }
-  })
-);
+hamburger.addEventListener('click', hideMenu);
+
+document.querySelectorAll('.navMenu li').forEach((n) => n.addEventListener('click', () => {
+  navMenu.style.left = '200%';
+}));
