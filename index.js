cb54cc939767903e20cd6351a07e3927762ea670
Ronald Mjonono <mjay@M-JAY.local>
2023-06-20 23:25:03 +0200

    add mobile menu

---
+++ index.js
@@ -0,0 +1,23 @@
+const cancelButton = document.querySelector(".cancelicon");
+const hamburger = document.querySelector(".bars");
+const navMenu = document.querySelector(".navMenu");
+const menuItem = document.getElementsByClassName("menuItem");
+const activePage = document.querySelectorAll;
+
+cancelButton.addEventListener("click", myFunction);
+
+function myFunction() {
+  navMenu.style.left = "200%";
+}
+
+hamburger.addEventListener("click", hideMenu);
+
+function hideMenu() {
+  navMenu.style.left = "0";
+}
+
+document.querySelectorAll(".navMenu li").forEach((n) =>
+  n.addEventListener("click", function () {
+    navMenu.style.left = "200%";
+  })
+);
