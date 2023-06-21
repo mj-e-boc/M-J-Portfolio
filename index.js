df602d18c65eabf0d233dd5d22d2611a0ff18edb
Tanmayi Manku <tanmayitanum@gmail.com>
2023-06-22 02:08:13 +0530

    Created an array with works card's content

--- index.js
+++ index.js
@@ -17,3 +17,55 @@ hamburger.addEventListener('click', hideMenu);
 document.querySelectorAll('.navMenu li').forEach((n) => n.addEventListener('click', () => {
   navMenu.style.left = '200%';
 }));
+
+const contentArray = [
+  {
+    title: 'Multi-Post Stories',
+    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. Has been the industry\'s standard.',
+    languages: ['CSS','HTML', 'Bootstrap', 'Ruby'],
+    livelink: '',
+    seesource: '',
+  },
+  {
+    title: 'Data DashboardHealthcare',
+    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. Has been the industry\'s standard.',
+    languages: ['HTML', 'Bootstrap', 'Ruby'],
+    livelink: '',
+    seesource: '',
+  },
+  {
+    title: 'Data DashboardHealthcare',
+    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. Has been the industry\'s standard.',
+    languages: ['HTML', 'Bootstrap', 'Ruby'],
+    livelink: '',
+    seesource: '',
+  },
+  {
+    title: 'Website Portfolio',
+    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. Has been the industry\'s standard.',
+    languages: ['HTML', 'Bootstrap', 'Ruby'],
+    livelink: '',
+    seesource: '',
+  },
+  {
+    title: 'Profesional Art Printing Data More',
+    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. Has been the industry\'s standard.',
+    languages: ['HTML', 'Bootstrap', 'Ruby'],
+    livelink: '',
+    seesource: '',
+  },
+  {
+    title: 'Data DashboardHealthcare',
+    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. Has been the industry\'s standard.',
+    languages: ['HTML', 'Bootstrap', 'Ruby'],
+    livelink: '',
+    seesource: '',
+  },
+  {
+    title: 'Website Portfolio',
+    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. Has been the industry\'s standard.',
+    languages: ['HTML', 'Bootstrap', 'Ruby'],
+    livelink: '',
+    seesource: '',
+  },
+];
