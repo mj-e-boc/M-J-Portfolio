9c9e72b539ce91c4cdfc6e028a4ef1841ff2778a
Tanmayi Manku <81354942+tan12082001@users.noreply.github.com>
2023-06-24 20:39:31 +0530

    Update validation.js

--- validation.js
+++ validation.js
@@ -2,7 +2,7 @@ const email = document.querySelector("#email");
 const errorElement = document.getElementById("#error");
 form.addEventListener("submit", (e) => {
   let message = "";
-  if (!email.value.Tolowercase()) message += "email must be small letters";
+  if (!email.value.Tolowercase()) message += "email must be in lowercase format.";
   e.preventDefault();
   errorElement.innerText = message;
 });
