7359ce633cd7454fa8d8caaa1506077769b560e2
Ronald Mjonono <mjay@M-JAY.local>
2023-06-24 18:46:56 +0200

    add form validation

--- validation.js
+++ validation.js
@@ -1,8 +1,22 @@
-const email = document.querySelector("#email");
-const errorElement = document.getElementById("#error");
-form.addEventListener("submit", (e) => {
-  let message = "";
-  if (!email.value.Tolowercase()) message += "email must be in lowercase format.";
-  e.preventDefault();
-  errorElement.innerText = message;
+const formy = document.querySelector('#form');
+const email = document.getElementById('imail');
+const erra = document.createElement('span');
+formy.appendChild(erra);
+const button = formy.querySelector('#submitbutton');
+formy.insertBefore(erra, button);
+erra.classList.add('error');
+
+formy.addEventListener('submit', (event) => {
+  event.preventDefault();
+  if (email.value.toLowerCase() !== email.value) {
+    email.classList.add('highlight');
+    erra.classList.add('inaction');
+    erra.textContent = 'Please Enter the email address in lowercase characters.';
+  } else {
+    email.classList.remove('highlight');
+    erra.classList.remove('inaction');
+    erra.textContent = '';
+    formy.submit();
+    formy.reset();
+  }
 });
